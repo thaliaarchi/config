905e0938d2b31b7ed70ca108d875d1c6f620ac03
Andrew Archibald <andrew@aarchibald.com>
2019-10-31 17:40:49 -0700

    Use GNU date on Darwin, determine repo root

--- scripts/commit-timed.sh
+++ scripts/commit-timed.sh
@@ -1,17 +1,27 @@
-staged_files=$(git diff --name-only --staged)
-max_nanoseconds=0
-max_date=""
+#!/bin/bash
+
+repo=$(git rev-parse --show-toplevel)
+staged_files=$(git -C "$repo" diff --name-only --staged)
 
 if [ ! "$staged_files" ]; then
   echo "No staged files"
   return 1
 fi
 
+DATE=date
+if uname | grep -q "Darwin"; then
+  # brew install coreutils
+  DATE=gdate
+fi
+
+max_nanoseconds=0
+max_date=""
+
 while read -r file; do
-  nanoseconds=$(date -r "$file" +'%s%N')
+  nanoseconds=$($DATE -r "$repo/$file" +'%s%N')
   if [ $nanoseconds -gt $max_nanoseconds ]; then
     max_nanoseconds=$nanoseconds
-    max_date=$(date -r "$file" +'%Y-%m-%d %T.%N %z')
+    max_date=$($DATE -r "$repo/$file" +'%Y-%m-%d %T.%N %z')
   fi
   #stat -c '%y' "$file" | tr -d '\n'
   #date -r "$file" +'%Y-%m-%d %T.%N %z' | tr -d '\n'
