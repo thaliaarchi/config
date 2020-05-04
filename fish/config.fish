892ed952b0956280ccc2e877f11735b22604752c
Andrew Archibald <andrew@aarchibald.com>
2020-05-04 01:47:47 -0700

    Add config.fish from rpi

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+set fish_greeting
+
+alias g=git
+alias clone="git clone"
+alias commit="git commit"
+alias pull="git pull"
+alias push="git push"
+
+alias ddg="lynx https://duckduckgo.com/lite"
+alias weather="curl wttr.in"
+
+set PATH $PATH /usr/local/go/bin
