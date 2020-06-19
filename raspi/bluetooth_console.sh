f8040f811697b0b934e1be1499c1b40c60283d46
Andrew Archibald <andrew@aarchibald.com>
2020-06-19 13:28:05 -0700

    Format Bluetooth console

--- raspi/bluetooth_console.sh
+++ raspi/bluetooth_console.sh
@@ -1,36 +1,48 @@
 #!/bin/bash -e
 
-echo Script for Raspberry Pi Zero W 1.1
-echo Linux raspberrypi 4.4.50+ #970 Mon Feb 20 19:12:50 GMT 2017
-echo Run once, reboot, and do not run again.
-echo Running...
+# Adapted from https://github.com/DrRowland/RPi-Bluetooth-Console/blob/master/setup.sh
+# Read more at https://hacks.mozilla.org/2017/02/headless-raspberry-pi-configuration-over-bluetooth/
+
+echo 'Raspberry Pi Bluetooth console'
+echo 'Run once, reboot, and do not run again.'
+echo
+
+if ! command -v bluetoothctl > /dev/null 2>&1; then
+  echo 'bluetoothctl must be installed. Run:'
+  echo
+  echo '    sudo apt-get install bluez'
+  exit 2
+fi
 
 sudo bluetoothctl <<EOF > /dev/null
 discoverable on
 EOF
 
-sudo sed -i: 's|^Exec.*toothd$| \
-ExecStart=/usr/lib/bluetooth/bluetoothd -C \
-ExecStartPost=/usr/bin/sdptool add SP \
-ExecStartPost=/bin/hciconfig hci0 piscan \
+# Enable Bluetooth services
+sudo sed -i: 's|^Exec.*toothd$|\
+ExecStart=/usr/lib/bluetooth/bluetoothd -C\
+ExecStartPost=/usr/bin/sdptool add SP\
+ExecStartPost=/bin/hciconfig hci0 piscan\
 |g' /lib/systemd/system/bluetooth.service
 
+# Enable the Bluetooth serial port from systemctl
 sudo cat <<EOF | sudo tee /etc/systemd/system/rfcomm.service > /dev/null
 [Unit]
 Description=RFCOMM service
 After=bluetooth.service
 Requires=bluetooth.service
- 
+
 [Service]
 ExecStart=/usr/bin/rfcomm watch hci0 1 getty rfcomm0 115200 vt100 -a pi
- 
+
 [Install]
 WantedBy=multi-user.target
 EOF
 
 sudo systemctl enable rfcomm
 
-echo Now reboot, pair and connect.
-echo E.g. In OSX, pair and in a terminal type:
-echo screen /dev/cu.raspberrypi-SerialPort 115200
-
+echo
+echo 'Now reboot, pair and connect.'
+echo 'In macOS, pair, then run:'
+echo
+echo '    screen /dev/cu.raspberrypi-SerialPort 115200'
