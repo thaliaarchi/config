8dfe32da4c7cc4e5ed1e51ce83811f449488bfde
Andrew Archibald <andrew@aarchibald.com>
2020-06-30 11:44:41 -0700

    Split Go version string

--- install/go_version.fish
+++ install/go_version.fish
@@ -5,12 +5,17 @@ if ! command -q jq
   exit 1
 end
 
-echo 'Fetching Go versions'
+echo 'Fetching Go releases'
 
 curl -sS -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/golang/go/git/refs/tags |
 jq -r 'reverse | .[] | .ref | select(test("refs/tags/go.+")) | ltrimstr("refs/tags/")'
 
 if command -q go
   echo 'Installed:'
-  go version
+  set v (go version)
+  echo $v
+  set parts (string match -r 'go version (go[^ ]+) ([^/]+)/(.+)' $v)
+  set installed_version $parts[2]
+  set installed_goos $parts[3]
+  set installed_goarch $parts[4]
 end
