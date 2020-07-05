dbe58e0f3332e73a4bbfdeeb4342b795fa14ade1
Andrew Archibald <andrew@aarchibald.com>
2020-07-04 21:54:39 -0700

    Vary tmux config location by version, add .bash_logout

--- link.fish
+++ link.fish
@@ -20,59 +20,72 @@ function link
     rm $dest
   end
   if test -e $dest
-    if test -z $compare_func
-      echo "File exists at $dest" >&2
-      return
-    else if eval $compare_func $dest $compare_args
-      rm $dest
-    else
-      return
-    end
+    echo "File already exists at $dest" >&2
+    return
   end
   set dest_dir (dirname $dest)
-  if ! test -d $dest_dir
+  if ! test -d "$dest_dir"
     mkdir -p -- $dest_dir
   end
   ln -s -- $config_dir/$src $dest
+end
+
+function remove_default
+  set dest $argv[1]
+  set default $argv[2]
+  if test -f $dest -a ! -L $dest
+    if cmp -s -- $default $dest
+      rm $dest
+    else
+      echo "File $dest is modified from default at $default" >&2
+    end
+  end
+end
+
+function remove_default_neofetch
+  set dest $argv[1]
+  if test -f $dest -a ! -L $dest
+    if neofetch --print_config 2>/dev/null | cmp -s -- $dest -
+      rm $dest
+    else if after_version (neofetch --version | head -n1) '4.0.2'
+      echo "File $dest is modified from `neofetch --print_config`" >&2
+    end
+  end
+end
+
+function after_version
+  set current $argv[1]
+  set min $argv[2]
+  set versions (string join \n $current $min | sort -V)
+  test "$versions[1]" = "$min"
+  return $status
 end
 
 if ! test -f $config_dir/dotfiles/bash/git-completion.bash
   wget 'https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash' -O $config_dir/dotfiles/bash/git-completion.bash
 end
 
-function file_default
-  set dest $argv[1]
-  set default $argv[2]
-  test -f "$dest" && cmp -s -- $default $dest
-  set cmp $status
-  if test $cmp -ne 0
-    echo "File $dest is modified from default at $default" >&2
-  end
-  return $cmp
-end
-
-function neofetch_default
-  set dest $argv[1]
-  neofetch --print_config 2>/dev/null | cmp -s -- $dest -
-  set cmp $status
-  if test $cmp -ne 0
-    set v (neofetch --version | head -n1)
-    echo "File $dest is modified from default or Neofetch is older than 4.0.2 ($v)" >&2
-  end
-  return $cmp
-end
-
 test -z "$XDG_CONFIG_HOME" && set XDG_CONFIG_HOME ~/.config
 
-# Suppress MOTD
-! test -f ~/.hushlogin && touch ~/.hushlogin
+remove_default ~/.bashrc /etc/skel/.bashrc
+remove_default ~/.bash_profile /etc/skel/.bash_profile
+remove_default ~/.bash_logout /etc/skel/.bash_logout
+remove_default ~/.screenrc /etc/skel/.screenrc
+remove_default_neofetch $XDG_CONFIG_HOME/neofetch/config.conf
 
-link dotfiles/neofetch.conf $XDG_CONFIG_HOME/neofetch/config.conf neofetch_default
-link dotfiles/shellcheckrc $XDG_CONFIG_HOME/shellcheckrc
+link dotfiles/bash/bashrc.bash ~/.bashrc
+link dotfiles/bash/bash_profile.bash ~/.bash_profile
+link dotfiles/bash/bash_logout.bash ~/.bash_logout
 link dotfiles/screenrc ~/.screenrc
-link dotfiles/tmux.conf ~/.config/tmux/tmux.conf # tmux >=3.1 (does not check $XDG_CONFIG_HOME)
-#link dotfiles/tmux.conf ~/.tmux.conf # tmux <3.1
-link dotfiles/fish/functions $XDG_CONFIG_HOME/fish/functions
+link dotfiles/neofetch/config.conf $XDG_CONFIG_HOME/neofetch/config.conf
+link dotfiles/shellcheckrc $XDG_CONFIG_HOME/shellcheckrc
+! test -f ~/.hushlogin && touch ~/.hushlogin # Suppress MOTD
+
+if after_version (tmux -V) 'tmux 3.1'
+  link dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf # not $XDG_CONFIG_HOME
+else
+  link dotfiles/tmux/tmux.conf ~/.tmux.conf
+end
+
 link dotfiles/fish/config.fish $XDG_CONFIG_HOME/fish/config.fish
-link dotfiles/bash/bashrc.bash ~/.bashrc file_default /etc/skel/.bashrc
-link dotfiles/bash/bash_profile.bash ~/.bash_profile file_default /etc/skel/.bash_profile
+link dotfiles/fish/functions $XDG_CONFIG_HOME/fish/functions
