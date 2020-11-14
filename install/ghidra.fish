072f5a4890f7af8d6a1fca73c089f2fc4ac7ee1c
Andrew Archibald <andrew@aarchibald.com>
2020-11-13 21:05:33 -0700

    Install Ghidra to ~/opt instead of /opt

--- install/ghidra.fish
+++ install/ghidra.fish
@@ -3,12 +3,12 @@
 # https://ghidra-sre.org/
 # https://ghidra-sre.org/InstallationGuide.html
 
-set VERSION 9.1.2
-set DATE 20200212
+set VERSION 9.2
+set DATE 20201113
 
 if ! test -f ghidra_{$VERSION}_PUBLIC_$DATE.zip
   wget https://ghidra-sre.org/ghidra_{$VERSION}_PUBLIC_$DATE.zip
 end
 unzip ghidra_{$VERSION}_PUBLIC_$DATE.zip
-sudo mv ghidra_{$VERSION}_PUBLIC /opt/ghidra_$VERSION
-ln -s /opt/ghidra_$VERSION/ghidraRun ~/bin/ghidra
+mv ghidra_{$VERSION}_PUBLIC ~/opt/ghidra_$VERSION
+ln -s ~/opt/ghidra_$VERSION/ghidraRun ~/bin/ghidra
