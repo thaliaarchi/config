69d6608c94c1a59f05d1a8da9bea30fb641b8dfa
Andrew Archibald <andrew@aarchibald.com>
2021-01-06 18:12:44 -0800

    Disable neofetch print

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -128,6 +128,6 @@ if test -e ~/.config/fish/config_local.fish
   source ~/.config/fish/config_local.fish
 end
 
-if status is-login && status is-interactive
-  command -q neofetch && neofetch
-end
+# if status is-login && status is-interactive
+#   command -q neofetch && neofetch
+# end
