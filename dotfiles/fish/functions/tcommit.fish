c4b8c135d4946c1bb2d51da1a1b8bbe4b3ecb681
Andrew Archibald <andrew@aarchibald.com>
2022-06-26 20:47:08 -0600

    Match bash tcommit in fish

--- dotfiles/fish/functions/tcommit.fish
+++ dotfiles/fish/functions/tcommit.fish
@@ -1,15 +1,20 @@
 function tcommit -d 'Git commit at file modification time' --wraps='git commit'
-  set -l toplevel (git rev-parse --show-toplevel)
-
-  set -l max_seconds 0
   set -l max_date ''
-  for file in (git diff --name-only -z --staged --diff-filter=d)
-    if test -e "$toplevel/$file"
-      set -l seconds (date -r $toplevel/$file +'%s')
-      if test "$seconds" -gt "$max_seconds"
-        set max_seconds $seconds
-        set max_date (date -r $toplevel/$file +'%Y-%m-%d %H:%M:%S %z')
+  if ! set -q GIT_AUTHOR_DATE || ! set -q GIT_COMMITTER_DATE
+    set -l toplevel (git rev-parse --show-toplevel)
+    set -l max_seconds -1
+    set -l max_file
+    for file in (git diff --name-only -z --staged --diff-filter=d)
+      if test -e "$toplevel/$file"
+        set -l seconds (date -r $toplevel/$file +'%s')
+        if test "$seconds" -gt "$max_seconds"
+          set max_seconds $seconds
+          set max_file $toplevel/$file
+        end
       end
+    end
+    if test $max_seconds != -1
+      set max_date (date -r $max_file +'%Y-%m-%d %H:%M:%S %z')
     end
   end
 
@@ -21,9 +26,13 @@ function tcommit -d 'Git commit at file modification time' --wraps='git commit'
   if set -q GIT_COMMITTER_DATE
     set cd $GIT_COMMITTER_DATE
   end
-  echo "Modify date: $max_date"
-  echo "Author date: $ad"
-  echo "Commit date: $cd"
+  if test $ad != $cd
+    echo "Modify date: $max_date"
+    echo "Author date: "(test -n "$ad" && echo $ad || echo now)
+    echo "Commit date: "(test -n "$cd" && echo $cd || echo now)
+  else
+    echo "Date: "(test -n "$ad" && echo $ad || echo now)
+  end
 
   GIT_AUTHOR_DATE=$ad \
   GIT_COMMITTER_DATE=$cd \
